0bc31ae764fdcc9dc6becff5ba71b6336e42373e
ozpinar0909 <ozpinar09@gmail.com>
2023-08-24 16:20:16 +0300

    Created AutoMapper

---
+++ Blog.Service/AutoMapper/ArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs;
+using Blog.Entity.Entities;
+
+namespace Blog.Service.AutoMapper
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDto, Article>().ReverseMap();
+        }
+    }
+}
---
+++ Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Service.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IArticleService article;
+
+        public HomeController(IArticleService article)
+        {
+            this.article = article;
+        }
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var articles = await article.GetAllArticleSync();
+            return View(articles);
+        }
+    }
+}
