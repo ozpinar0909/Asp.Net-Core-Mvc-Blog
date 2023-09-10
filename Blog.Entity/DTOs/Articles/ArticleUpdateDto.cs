155b7982542b9ec3b75cb93cba4e6cf0c4427ace
ozpinar0909 <ozpinar09@gmail.com>
2023-09-10 21:19:03 +0300

    Created ArticleUpdatePage

---
+++ Blog.Entity/DTOs/Articles/ArticleUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Entity.DTOs.Categories;
+
+namespace Blog.Entity.DTOs.Articles
+{
+    public class ArticleUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid CategoryId { get; set; }
+        public IList<CategoryDto> Categories { get; set; }
+    }
+}
