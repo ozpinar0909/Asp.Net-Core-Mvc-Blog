5d8bb0a93f4396da7b5f4a6e85a9c46a96dcc637
ozpinar0909 <ozpinar09@gmail.com>
2023-09-12 13:09:37 +0300

    Created Toastr Notify

---
+++ Blog.Web/ResultMessages/Messages.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Web.ResultMessages
+{
+    public class Messages
+    {
+        public static class Article
+        {
+            public static string Add(string articleTitle)
+            {
+                return $"{articleTitle} başlıklı makale başarıyla eklenmiştir.";
+            }
+            public static string Update(string articleTitle)
+            {
+                return $"{articleTitle} başlıklı makale başarıyla güncellenmiştir.";
+            }
+        }
+    }
+}
